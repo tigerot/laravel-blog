a78162264ec64fcb5df37586be166b7c2f9b8604
Suat Kuran <suatkuran@gmail.com>
2024-03-19 18:19:20 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Welcome to our Blog!
+                </h1>
+                <a 
+                    href="/blog"
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+        </div>
+
+        <div class="m-auto sm:m-auto text-left w-4/5 block">
+            <h2 class="text-3xl font-extrabold text-gray-600">
+                Want to learn about Aesthetic Operations?
+            </h2>
+            
+            <p class="py-8 text-gray-500 text-s">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+            </p>
+
+            <p class="font-extrabold text-gray-600 text-s pb-9">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+            </p>
+
+            <a 
+                href="/blog"
+                class="uppercase text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl" style="background-color: #13a89e;">
+                Find Out More
+            </a>
+        </div>
+    </div>
+
+    <div class="text-center p-15 text-white" style="background-color: #13a89e;">
+        <h2 class="text-2xl pb-5 text-l"> 
+            I'm an expert in...
+        </h2>
+
+        <span class="font-extrabold block text-4xl py-1">
+            Aesthetic Operations
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Hair Transplant
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Body Aethetics
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Other Aesthetics
+        </span>
+    </div>
+
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,90 @@
+<footer class="py-20 mt-20" style="background-color: #13a89e;">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-black-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-black-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-black-100">
+                Find Us
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-black-400">
+                <li class="pb-1">
+                    <a href="/">
+                        What we do
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Address
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Phone
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-black-100">
+                Latest posts
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-black-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Aesthetic
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Body Aesthetic
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Hair Transplant
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Beauty Aesthetic
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
